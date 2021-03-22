3d64a55c606617a75a8e451624fe27448b59acba
prathjan <prathjan@cisco.com>
2021-03-22 16:28:32 -0700

    helm init

--- main.tf
+++ main.tf
@@ -33,25 +33,25 @@ data "terraform_remote_state" "iksws" {
 #  moid = ""
 #}
 
-provider "helm" {
-  kubernetes {
-    host = local.kube_config.clusters[0].cluster.server
-    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
-    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
-    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
-  }
-}
-
-
-
 #provider "helm" {
-# kubernetes {
-#  host = data.terraform_remote_state.iksws.outputs.kube_config.clusters[0].cluster.server
-#  client_certificate = base64decode(data.terraform_remote_state.iksws.outputs.kube_config.users[0].user.client-certificate-data)
-#  client_key = base64decode(data.terraform_remote_state.iksws.outputs.kube_config.users[0].user.client-key-data)
-#  cluster_ca_certificate = base64decode(data.terraform_remote_state.iksws.outputs.kube_config.clusters[0].cluster.certificate-authority-data)
-# }
+#  kubernetes {
+#    host = local.kube_config.clusters[0].cluster.server
+#    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+#    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+#    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+#  }
 #}
+
+
+
+provider "helm" {
+ kubernetes {
+  host = data.terraform_remote_state.iksws.outputs.kube_config.clusters[0].cluster.server
+  client_certificate = base64decode(data.terraform_remote_state.iksws.outputs.kube_config.users[0].user.client-certificate-data)
+  client_key = base64decode(data.terraform_remote_state.iksws.outputs.kube_config.users[0].user.client-key-data)
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.iksws.outputs.kube_config.clusters[0].cluster.certificate-authority-data)
+ }
+}
 
 
 variable "api_private_key" {
@@ -70,10 +70,10 @@ variable "iksclustername" {
   type = string
 }
 
-locals {
-  kube_config = yamldecode(base64decode(data.terraform_remote_state.iksws.outputs.kube_config))
+#locals {
+#  kube_config = yamldecode(base64decode(data.terraform_remote_state.iksws.outputs.kube_config))
   #kube_config = yamldecode(base64decode(data.intersight_kubernetes_cluster.ikscluster.results[0].kube_config))
-}
+#}
 
 
 resource helm_release nginx_ingress {
