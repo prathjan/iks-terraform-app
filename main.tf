4c2cb214750cd313fb0b80288ce1c3e7f4f040c3
prathjan <prathjan@cisco.com>
2021-03-22 14:49:20 -0700

    Using cluster kubeconfig directly

--- main.tf
+++ main.tf
@@ -1,55 +1,34 @@
 # Intersight Provider Information
 terraform {
   required_providers {
-#    intersight = {
-#      source = "CiscoDevNet/intersight"
-#      version = "1.0.2"
-#    }
-    helm = {
-      source = "hashicorp/helm"
-      version = "2.0.3"
+    intersight = {
+      source = "CiscoDevNet/intersight"
+      version = "1.0.3"
     }
   }
 }
 
+provider "intersight" {
+  apikey        = var.api_key_id
+  secretkey = var.api_private_key
+  endpoint      = var.api_endpoint
+}
 
-#provider "intersight" {
-#  apikey        = var.api_key_id
-#  secretkey = var.api_private_key
-#  endpoint      = var.api_endpoint
-#}
-
-#data "intersight_kubernetes_cluster" "ikscluster" {
-#  name  = var.iksclustername
-#  moid = ""
-#}
+data "intersight_kubernetes_cluster" "ikscluster" {
+  name  = var.iksclustername
+  moid = ""
+}
 
 provider "helm" {
   kubernetes {
-    host = var.host 
-    client_certificate = base64decode(var.clcert) 
-    client_key = base64decode(var.clkey) 
-    cluster_ca_certificate = base64decode(var.cacert) 
-#    host = local.kube_config.clusters[0].cluster.server
-#    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
-#    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
-#    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+    host = local.kube_config.clusters[0].cluster.server
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
   }
 }
 
 
-variable "host" {
-  type = string
-}
-variable "clcert" {
-  type = string
-}
-variable "clkey" {
-  type = string
-}
-variable "cacert" {
-  type = string
-}
 variable "api_private_key" {
   type = string
 }
@@ -66,9 +45,10 @@ variable "iksclustername" {
   type = string
 }
 
-#locals {
-#  kube_config = yamldecode(base64decode(data.intersight_kubernetes_cluster.ikscluster.results[0].kube_config))
-#}
+locals {
+  kube_config = yamldecode(base64decode(data.intersight_kubernetes_cluster.ikscluster.results[0].kube_config))
+}
+
 
 resource helm_release nginx_ingress {
   name       = "nginx-ingress-controller"
@@ -82,7 +62,3 @@ resource helm_release nginx_ingress {
   }
 }
 
-
-output "lb_ip" {
-	value = helm_release.nginx_ingress.status.0.load_balancer.0.ingress.0.hostname
-}
