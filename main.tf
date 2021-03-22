c84a6d5b086130851bb9bbe6f00cabd2f1e79753
prathjan <prathjan@cisco.com>
2021-03-22 15:47:08 -0700

    changes to accessing o/p

--- main.tf
+++ main.tf
@@ -33,13 +33,24 @@ data "terraform_remote_state" "iksws" {
 #  moid = ""
 #}
 
+#provider "helm" {
+#  kubernetes {
+#    host = local.kube_config.clusters[0].cluster.server
+#    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+#    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+#    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+#  }
+#}
+
+
+
 provider "helm" {
-  kubernetes {
-    host = local.kube_config.clusters[0].cluster.server
-    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
-    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
-    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
-  }
+ kubernetes {
+  host = data.terraform_remote_state.iksws.outputs.kube_config.clusters[0].cluster.server
+  client_certificate = base64decode(data.terraform_remote_state.iksws.outputs.kube_config.users[0].user.client-certificate-data)
+  client_key = base64decode(data.terraform_remote_state.iksws.outputs.kube_config.users[0].user.client-key-data)
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.iksws.outputs.kube_config.clusters[0].cluster.certificate-authority-data)
+ }
 }
 
 
